f0e95d734244e792d2c289b13a253cd8e13e6e6c
Amir Mohammadi <amiremohamadi@yahoo.com>
2025-01-19 15:27:41 +0330

    add challenge sample

---
+++ countme/main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+
+application = Flask(__name__)
+
+global sum_of_requests
+sum_of_requests = 0
+
+@application.route('/count')
+def count():
+    global sum_of_requests
+    return str(sum_of_requests)
+
+
+@application.route('/', methods=['POST'])
+def index():
+    global sum_of_requests
+    in_number = int(request.stream.read())
+    sum_of_requests += in_number
+    return str(sum_of_requests)
+
+
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=80, debug=True)
